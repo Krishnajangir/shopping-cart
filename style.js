aa2e7a6d2255de03069713e9692d8b1c96a6b4f7
krishna <krishna.jangir@publicissapient.com>
2021-04-30 09:52:54 +0530

    stable

--- style.js
+++
@@ -1,7 +0,0 @@
-
-getCartItemDetails = () => {
-      window.location.href = "http://127.0.0.1:5500/views/home.html";
-      let item = document.getElementById('items');
-      item.innerHTML = 5 + ' Items';
-}
-getCartItemDetails()
